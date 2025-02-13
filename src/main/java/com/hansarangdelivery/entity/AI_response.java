dd34803b948f703676b1179a65db8c5b3703a24f
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-02-13 22:02:49 +0900

    feat : AI response 엔티티 추가

---
+++ src/main/java/com/hansarangdelivery/entity/AI_response.java
@@ -0,0 +1,33 @@
+package com.hansarangdelivery.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "p_AI_response")
+@NoArgsConstructor
+public class AI_response extends TimeStamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ai_response_id", columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id; // 고유 식별자
+
+    @Column(name = "request_test", columnDefinition = "TEXT", nullable = false)
+    private String request_text;
+
+    @Column(name = "request_test", columnDefinition = "TEXT")
+    private String response_text;
+
+}
+
+
