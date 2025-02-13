794e81a1d6bf9d4a876556e9eb78efbef01943f6
zapzookj <zapzook@naver.com>
2025-02-13 12:57:31 +0900

    init commit

---
+++ src/main/java/com/hansarangdelivery/HansarangDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.hansarangdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansarangDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HansarangDeliveryApplication.class, args);
+    }
+
+}
