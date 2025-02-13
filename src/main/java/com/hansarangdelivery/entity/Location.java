0142bf1c96683c36dfded60370a3ea71e9c29214
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-02-13 22:15:33 +0900

    refactor : @Entity 추가

--- src/main/java/com/hansarangdelivery/entity/Location.java
+++ src/main/java/com/hansarangdelivery/entity/Location.java
@@ -2,6 +2,7 @@ package com.hansarangdelivery.entity;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 @Table(name = "p_location")
 public class Location extends TimeStamped{
     @Id
