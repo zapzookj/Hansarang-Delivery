1b6cfa0f74c0736fb18a0a9cea5c69d09f129d07
zapzookj <zapzook@naver.com>
2025-02-14 20:53:46 +0900

    feat : User 도메인 API 구현

--- src/main/java/com/hansarangdelivery/config/WebSecurityConfig.java
+++ src/main/java/com/hansarangdelivery/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
     private final JwtUtil jwtUtil;
--- src/main/java/com/hansarangdelivery/controller/UserController.java
+++ src/main/java/com/hansarangdelivery/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.hansarangdelivery.controller;
 
-import com.hansarangdelivery.dto.ResultResponseDto;
-import com.hansarangdelivery.dto.SignupRequestDto;
+import com.hansarangdelivery.dto.*;
+import com.hansarangdelivery.security.UserDetailsImpl;
 import com.hansarangdelivery.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +19,50 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ResultResponseDto> SignUp(@Valid @RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<ResultResponseDto<Void>> SignUp(@Valid @RequestBody SignupRequestDto requestDto) {
         userService.signup(requestDto);
-        return ResponseEntity.status(200).body(new ResultResponseDto("회원가입 성공", 200));
+        return ResponseEntity.status(200).body(new ResultResponseDto<>("회원가입 성공", 200));
     }
+
+    @GetMapping("/{userId}")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    public ResponseEntity<ResultResponseDto<UserResponseDto>> getProfile(@PathVariable("userId") Long userId) {
+        UserResponseDto responseDto = userService.getProfile(userId);
+        return ResponseEntity.status(200).body(new ResultResponseDto<>("조회 성공", 200, responseDto));
+    }
+
+    @GetMapping()
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    public ResponseEntity<ResultResponseDto<Page<UserResponseDto>>> getAllProfile(@RequestBody PageRequestDto requestDto) {
+        Page<UserResponseDto> responseDtoPage = userService.getAllProfile(requestDto);
+        return ResponseEntity.status(200).body(new ResultResponseDto<>("조회 성공",200, responseDtoPage));
+    }
+
+    @GetMapping("/my-profile")
+    public ResponseEntity<ResultResponseDto<UserResponseDto>> getMyProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        UserResponseDto responseDto = new UserResponseDto(userDetails.getUser());
+        return ResponseEntity.status(200).body(new ResultResponseDto<>("조회 성공", 200, responseDto));
+    }
+
+    @PutMapping("/my-profile")
+    public ResponseEntity<ResultResponseDto<Void>> updateProfile(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                                 @RequestBody UserUpdateDto requestDto) {
+        userService.updateProfile(userDetails.getUser().getId(), requestDto);
+        return ResponseEntity.status(200).body(new ResultResponseDto<>("회원 정보 수정 성공", 200));
+    }
+
+    @PutMapping("/{userId}")
+    @PreAuthorize("hasRole('ROLE_MASTER')")
+    public ResponseEntity<ResultResponseDto<Void>> updateRole(@PathVariable("userId")Long userId) {
+        userService.updateRole(userId);
+        return ResponseEntity.status(200).body(new ResultResponseDto<>("권한 변경 성공", 200));
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<ResultResponseDto<Void>> deleteUser(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                              @RequestParam(value = "userId", required = false) Long userId) {
+        userService.deleteUser(userDetails.getUser(), userId);
+        return ResponseEntity.status(200).body(new ResultResponseDto<>("회원 탈퇴 성공", 200));
+    }
+
 }
