c91bed58c5571574061230c8190157cd9c4f4103
gwongilnam <nimpa3201@naver.com>
2025-02-16 21:26:39 +0900

    fix : entity 의존성 주입 방식 통일

--- src/main/java/com/hansarangdelivery/entity/OrderType.java
+++ src/main/java/com/hansarangdelivery/entity/OrderType.java
@@ -3,29 +3,18 @@ package com.hansarangdelivery.entity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@Getter
+@AllArgsConstructor
 public enum OrderType {
     ONLINE("온라인 주문"),
     OFFLINE("대면 주문");
 
     private final String description;
 
-    OrderType(String description) {
-        this.description = description;
-    }
-
-    @JsonValue
-    public String getDescription() {
-        return description;
-    }
-
-    @JsonCreator
-    public static OrderType fromString(String value) {
-        for (OrderType type : OrderType.values()) {
-            if (type.name().equalsIgnoreCase(value)) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Unknown OrderType: " + value);
-    }
 }
+
