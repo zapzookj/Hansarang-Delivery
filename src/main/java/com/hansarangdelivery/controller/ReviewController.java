d45082ddce0800274e924818a1481e7d2eca9b30
Jeongwook Woo <dnwjddnr115@naver.com>
2025-02-16 18:47:51 +0900

    Review Create 구현

---
+++ src/main/java/com/hansarangdelivery/controller/ReviewController.java
@@ -0,0 +1,24 @@
+package com.hansarangdelivery.controller;
+
+import com.hansarangdelivery.dto.ResultResponseDto;
+import com.hansarangdelivery.dto.ReviewRequestDto;
+import com.hansarangdelivery.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResultResponseDto<Void> addReview(@RequestBody ReviewRequestDto requestDto) {
+        return reviewService.addReview(requestDto);
+    }
+
+    @GetMapping
+    public ResultResponseDto<List<ReviewResponseDto>>
+}
