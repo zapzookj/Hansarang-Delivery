ee6e74c3b98ade2c04eab63a0f8f6cb26d2617a9
gwongilnam <nimpa3201@naver.com>
2025-02-16 21:48:06 +0900

    Resolve merge conflict in OrderType.java

--- src/main/java/com/hansarangdelivery/entity/OrderType.java
+++ src/main/java/com/hansarangdelivery/entity/OrderType.java
@@ -1,46 +1,16 @@
 package com.hansarangdelivery.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-<<<<<<< Updated upstream
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 
 @Getter
 @AllArgsConstructor
-=======
 
->>>>>>> Stashed changes
 public enum OrderType {
     ONLINE("온라인 주문"),
     OFFLINE("대면 주문");
 
     private final String description;
 
-<<<<<<< Updated upstream
 }
-
-=======
-    OrderType(String description) {
-        this.description = description;
-    }
-
-    @JsonValue
-    public String getDescription() {
-        return description;
-    }
-
-    @JsonCreator
-    public static OrderType fromString(String value) {
-        for (OrderType type : OrderType.values()) {
-            if (type.name().equalsIgnoreCase(value)) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Unknown OrderType: " + value);
-    }
-}
->>>>>>> Stashed changes
