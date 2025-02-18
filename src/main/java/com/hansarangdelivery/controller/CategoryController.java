2f8adfb9e66ee38144fef7bb0063a2e31f8212e3
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-02-18 16:06:32 +0900

    refactor : 규칙에 맞게 수정

--- src/main/java/com/hansarangdelivery/controller/CategoryController.java
+++ src/main/java/com/hansarangdelivery/controller/CategoryController.java
@@ -1,11 +1,13 @@
 package com.hansarangdelivery.controller;
 
 import com.hansarangdelivery.dto.CategoryRequestDto;
+import com.hansarangdelivery.dto.CategoryResponseDto;
 import com.hansarangdelivery.dto.ResultResponseDto;
 import com.hansarangdelivery.entity.Category;
 import com.hansarangdelivery.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,44 +25,33 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
-//    @PreAuthorize("hasRole('MANAGER')")
-    public ResponseEntity<ResultResponseDto> create(@Valid @RequestBody CategoryRequestDto categoryRequestDto) {
-        categoryService.createCategory(categoryRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new ResultResponseDto("카테고리 생성 성공", 200));
+    public ResultResponseDto create(@RequestBody @Valid CategoryRequestDto categoryRequestDto) {
+        ResultResponseDto result = categoryService.createCategory(categoryRequestDto);
+        return result;
     }
 
     @PutMapping("/{categoryId}")
-    public ResponseEntity<ResultResponseDto> updateCategory(@PathVariable UUID categoryId, @RequestBody Category updatedCategory) {
-        Optional<Category> existingCategory = categoryService.getCategoryById(categoryId);
-
-        if (existingCategory.isPresent()) {
-            // 이름 중복 확인
-            if (categoryService.isCategoryNameExists(updatedCategory.getName(), categoryId)) {
-                return ResponseEntity.ok(new ResultResponseDto("이미 존재하는 카테고리 이름입니다.", 204));
-            }
-
-            Category categoryToUpdate = existingCategory.get();
-            categoryToUpdate.setName(updatedCategory.getName());
-            categoryService.saveCategory(categoryToUpdate);
-
-            return ResponseEntity.ok(new ResultResponseDto("카테고리 변경 성공", 200));
-        } else {
-            return ResponseEntity.ok(new ResultResponseDto("카테고리를 찾을 수 없습니다.", 204));
-        }
+    public ResultResponseDto updateCategory(@PathVariable UUID categoryId, @RequestBody CategoryRequestDto categoryRequestDto) {
+        ResultResponseDto result = categoryService.updateCategory(categoryId,categoryRequestDto);
+        return result;
     }
 
 
     @DeleteMapping("/{categoryId}")
-    public ResponseEntity<ResultResponseDto> deleteCategory(@PathVariable UUID categoryId) {
-        categoryService.deleteCategory(categoryId);
-        return ResponseEntity.ok(new ResultResponseDto("카테고리 삭제 성공", 204)); // 삭제 성공 응답
+    public ResultResponseDto deleteCategory(@PathVariable UUID categoryId) {
+        ResultResponseDto result = categoryService.deleteCategory(categoryId);
+        return result;
     }
 
     @GetMapping("/search")
-    public ResponseEntity<ResultResponseDto> searchAllCategoryNames() {
-        List<String> categoryNames = categoryService.getAllCategoryNames(); // 전체 카테고리 이름 조회
-        return ResponseEntity.ok(new ResultResponseDto("전체 카테고리 이름 조회 성공", 200,categoryNames)); // 결과 반환
+    public ResultResponseDto<Page<CategoryResponseDto>> searchAllCategories(
+        @RequestParam(defaultValue = "1") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "createdAt") String sort,
+        @RequestParam(defaultValue = "desc") String direction
+        ) {
+        ResultResponseDto result = categoryService.getAllCategory(page,size,sort,direction); // 전체 카테고리 이름 조회
+        return result;
     }
 
 
--- src/main/java/com/hansarangdelivery/repository/CategoryRepository.java
+++ src/main/java/com/hansarangdelivery/repository/CategoryRepository.java
@@ -2,13 +2,17 @@ package com.hansarangdelivery.repository;
 
 import com.hansarangdelivery.entity.Category;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    boolean existsByNameAndDeletedAtIsNull(String name);
+    Optional<Category> findByIdAndDeletedAtIsNull(UUID id);
     Optional<Category> findByName(String name);
 
-
+    Page<Category> findByDeletedAtIsNull(Pageable pageable);
 }
