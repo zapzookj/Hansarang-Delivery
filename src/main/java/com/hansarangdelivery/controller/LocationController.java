b815d33d2efbefa8a71148e59fa256cb9d950d44
zapzookj <zapzook@naver.com>
2025-02-20 15:55:22 +0900

    test : locationId를 통한 위치 데이터 조회 API 구현

--- src/main/java/com/hansarangdelivery/controller/LocationController.java
+++ src/main/java/com/hansarangdelivery/controller/LocationController.java
@@ -3,14 +3,13 @@ package com.hansarangdelivery.controller;
 import com.hansarangdelivery.dto.LocationRequestDto;
 import com.hansarangdelivery.dto.LocationResponseDto;
 import com.hansarangdelivery.dto.ResultResponseDto;
+import com.hansarangdelivery.dto.RoadNameResponseDto;
 import com.hansarangdelivery.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +18,25 @@ public class LocationController {
 
     private final LocationService locationService;
 
+    // 키워드로 위치 데이터 검색 API
     @GetMapping("")
-    public ResponseEntity<ResultResponseDto<Page<LocationResponseDto>>> searchLocations(@ModelAttribute LocationRequestDto requestDto) {
+    public ResultResponseDto<Page<LocationResponseDto>> searchLocations(@ModelAttribute LocationRequestDto requestDto) {
         Page<LocationResponseDto> responseDtoPage = locationService.searchLocations(requestDto);
-        return ResponseEntity.status(200).body(new ResultResponseDto<>("조회 성공", 200, responseDtoPage));
+        return new ResultResponseDto<>("조회 성공", 200, responseDtoPage);
     }
+
+    // locationId로 도로명 주소를 반환하는 API
+    @GetMapping("/{locationId}")
+    public ResultResponseDto<RoadNameResponseDto> readRoadName(@PathVariable("locationId") UUID locationId) {
+        RoadNameResponseDto responseDto = locationService.readRoadName(locationId);
+        return new ResultResponseDto<>("조회 성공", 200, responseDto);
+    }
+
+    // locationId로 위치 상세 정보를 조회하는 API
+    @GetMapping("/detail/{locationId}")
+    public ResultResponseDto<LocationResponseDto> readLocation(@PathVariable("locationId") UUID locationId) {
+        LocationResponseDto responseDto = locationService.readLocation(locationId);
+        return new ResultResponseDto<>("조회 성공", 200, responseDto);
+    }
+
 }
--- src/main/java/com/hansarangdelivery/service/LocationService.java
+++ src/main/java/com/hansarangdelivery/service/LocationService.java
@@ -2,6 +2,9 @@ package com.hansarangdelivery.service;
 
 import com.hansarangdelivery.dto.LocationRequestDto;
 import com.hansarangdelivery.dto.LocationResponseDto;
+import com.hansarangdelivery.dto.RoadNameResponseDto;
+import com.hansarangdelivery.entity.Location;
+import com.hansarangdelivery.exception.ResourceNotFoundException;
 import com.hansarangdelivery.repository.LocationRepository;
 import com.hansarangdelivery.repository.LocationRepositoryQuery;
 import java.util.UUID;
@@ -28,5 +31,21 @@ public class LocationService {
 
     public boolean existsById(UUID locationId){
         return locationRepository.existsById(locationId);
+    }
+
+    public RoadNameResponseDto readRoadName(UUID locationId) {
+        Location location = findLocation(locationId);
+        return new RoadNameResponseDto(location.getRoadNameCode());
+    }
+
+    public LocationResponseDto readLocation(UUID locationId) {
+        Location location = findLocation(locationId);
+        return new LocationResponseDto(location);
+    }
+
+    private Location findLocation(UUID locationId) {
+        return locationRepository.findById(locationId).orElseThrow(
+            () -> new ResourceNotFoundException("해당 Id를 가진 Location 은 존재하지 않습니다.")
+        );
     }
 }
