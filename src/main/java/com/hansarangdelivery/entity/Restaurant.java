5175275fa5557e5b2ead3343a5a7fee49e82815e
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-02-20 16:01:06 +0900

    feat : restaurant 도메인 삭제된 부분 조회 제한 및 가게 이름의 글자 길이 제한

--- src/main/java/com/hansarangdelivery/entity/Restaurant.java
+++ src/main/java/com/hansarangdelivery/entity/Restaurant.java
@@ -6,18 +6,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "p_restaurant")
+@Where(clause = "deleted_at IS NULL")
 @NoArgsConstructor
 public class Restaurant extends TimeStamped{
     @Id
@@ -32,7 +32,7 @@ public class Restaurant extends TimeStamped{
     private UUID category; // 카테고리
 
     @Column(name="owner_id",nullable = false)
-    private UUID owner; // 소유자
+    private Long owner; // 소유자
 
     @Column(name="location_id",nullable = false)
     private UUID location; // 위치
@@ -40,13 +40,21 @@ public class Restaurant extends TimeStamped{
     @Column(name = "status", nullable = false)
     private boolean status; // 가게 상태 (운영 중 여부)
 
-    public Restaurant(String name, UUID categoryId, UUID ownerId, UUID locationId) {
-        super();
+    public Restaurant(String name, UUID categoryId, Long ownerId, UUID locationId) {
+//      테스트 코드 위해 남겨놓음
         this.name = name;
         this.category = categoryId;
         this.owner = ownerId; //
         this.location = locationId; // locationId로 초기화
         this.status = false; // 초기 상태를 '닫음'으로 설정
+    }
+
+    public Restaurant(RestaurantRequestDto requestDto){
+        this.name = requestDto.getName();
+        this.category = requestDto.getCategory_id();
+        this.owner = requestDto.getOwner_id();
+        this.location = requestDto.getLocation_id();
+        this.status = false;
     }
 
     public boolean getStatus(){
