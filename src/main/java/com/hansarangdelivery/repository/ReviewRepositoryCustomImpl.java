98810b334c431d33c8033aca7d95c540259c9b00
Jeongwook Woo <dnwjddnr115@naver.com>
2025-02-20 12:06:52 +0900

    Review 페이지 응답 정렬조건 수정

--- src/main/java/com/hansarangdelivery/repository/ReviewRepositoryCustomImpl.java
+++ src/main/java/com/hansarangdelivery/repository/ReviewRepositoryCustomImpl.java
@@ -64,13 +64,21 @@ public class ReviewRepositoryCustomImpl implements ReviewRepositoryCustom {
 
         QReview review = QReview.review;
 
-        List<Review> reviewList = queryFactory
+        Sort sort = pageable.getSort();
+
+        JPAQuery<Review> query = queryFactory
             .selectFrom(review)
             .where(review.createdBy.eq(userId))
-            .orderBy(review.createdAt.desc(), review.updatedAt.desc())
             .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .fetch();
+            .limit(pageable.getPageSize());
+
+        if(sort.iterator().next().isAscending()){
+            query.orderBy(review.createdAt.asc(), review.updatedAt.asc());
+        } else {
+            query.orderBy(review.createdAt.desc(), review.updatedAt.desc());
+        }
+
+        List<Review> reviewList = query.fetch();
 
         long total = queryFactory
             .selectFrom(review)
