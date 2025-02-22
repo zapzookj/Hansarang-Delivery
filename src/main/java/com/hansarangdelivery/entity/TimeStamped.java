53d1501bc06acb13fb235dd06383ee034d55f23d
zapzookj <zapzook@naver.com>
2025-02-22 15:06:31 +0900

    fix : createdAt,updatedAt (LocalDateTime) 데이터 역직렬화 에러 해결 (@JsonIgnore 처리)

--- src/main/java/com/hansarangdelivery/entity/TimeStamped.java
+++ src/main/java/com/hansarangdelivery/entity/TimeStamped.java
@@ -1,5 +1,6 @@
 package com.hansarangdelivery.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -19,10 +20,12 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeStamped {
 
+    @JsonIgnore
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @JsonIgnore
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
