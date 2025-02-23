0cc580d53e81406e9cdbe2bde51fe6f11b0d649c
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-02-23 16:44:57 +0900

    refactor : 엔드포인트 규칙에 맞게 수정, 그에 따른 테스트 수정

--- src/main/java/com/hansarangdelivery/controller/RestaurantController.java
+++ src/main/java/com/hansarangdelivery/controller/RestaurantController.java
@@ -65,9 +65,9 @@ public class RestaurantController {
         return new ResultResponseDto<>("가게 삭제 성공", 200,result);
     }
 
-    @GetMapping("/search")
+    @GetMapping("/")
     public ResultResponseDto<Page<RestaurantResponseDto>> searchRestaurants(
-    // 레스토랑 리스트 검색, search가 포함된 음식점을 찾는데 사용될 예정
+    // 레스토랑 리스트 검색
         Pageable pageable,
         @RequestParam(required = false) String search,
         @RequestParam(required = false) String category) {
